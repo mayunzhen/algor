2b5bebb3f0fd30317a49288bd7c6ff7b56ac934c
mayunzhen <632940781@qq.com>
2019-05-07 19:05:15 +0800

    add leetcode

--- src/main/java/com/example/algorithm/leetcode/medium/GenerateParentheses.java
+++ src/main/java/com/example/algorithm/leetcode/medium/GenerateParentheses.java
@@ -5,14 +5,28 @@ import java.util.List;
 
 public class GenerateParentheses {
     public static List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<>();
+        List<String> res = new ArrayList<>();
 
-        
+        helper("",res,n,0,0);
+        return res;
+    }
+    public static void helper(String current,List<String> res,int n,int left,int right){
+        if(left < right)
+            return;
+        if(left == n && right == n){
+            res.add(current);
+            return;
+        }
+        if(left < n){
+            helper(current+"(",res,n,left+1,right);
+        }
+        if(right < n){
+            helper(current+")",res,n,left,right+1);
+        }
 
-        return list;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(generateParenthesis(3));
     }
 }
